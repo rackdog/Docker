a2a71e66b30ed6c7fd92e16b7986c865d2020660
Burke Butler <exoscloudmanagement@gmail.com>
2025-06-30 09:45:14 -0400

    bug fix

---
+++ create-hardware-db.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+# create-hardware-db.sh
+#
+# Connects to the 'mysql' container as root, creates database+user, grants privileges.
+
+# Configuration (temporary credentials)
+MYSQL_CONTAINER="mysql"
+MYSQL_ROOT_PASSWORD="NX4U7ogcyX9dC7N33eGc"
+HW_DB="hardware"
+HW_USER="hw_user"
+HW_PASS="K7Mo3XGWHRqbuAx"
+
+# Run the SQL statements inside the container
+docker exec -i "${MYSQL_CONTAINER}" mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" <<EOSQL
+-- 1) Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS \`${HW_DB}\`;
+
+-- 2) Create (or update) the user and set its password
+CREATE USER IF NOT EXISTS '${HW_USER}'@'%' IDENTIFIED BY '${HW_PASS}'
+  /* for MySQL ≥8.0 this will also update the password if the user already exists */;
+
+-- 3) Grant all privileges on the new database
+GRANT ALL PRIVILEGES ON \`${HW_DB}\`.* TO '${HW_USER}'@'%';
+
+-- 4) Apply changes
+FLUSH PRIVILEGES;
+EOSQL
+
+echo "Database '${HW_DB}' and user '${HW_USER}'@'%' created (or updated) successfully."
